d113109ecef762f036349811d181f4d05de645e8
Ivan <263476263476v@gmail.com>
2021-02-01 06:22:01 +0700

    Добавление dropRight

--- src/servers.js
+++ src/servers.js
@@ -4,3 +4,59 @@ function server_creation() {
   let images = document.getElementById("images");
   images.append(div);
 }
+
+function first_servers() {
+  for (i = 0; i < 3; i++) server_creation();
+}
+
+let k = 0;
+
+function expand_narrow() {
+  if (k % 2 == 0) {
+    $("#panel").animate(
+      {
+        "min-width": "250px",
+        "max-width": "250px",
+      },
+      200
+    );
+    $("#servers").animate(
+      {
+        opacity: "0",
+      },
+      100
+    );
+    $("#drop_right").animate(
+      {
+        "min-width": "250px",
+        "max-width": "250px",
+      },
+      200
+    );
+  } else {
+    $("#panel").animate(
+      {
+        "min-width": "60px",
+        "max-width": "60px",
+      },
+      200
+    );
+    $("#servers").animate(
+      {
+        opacity: "1",
+      },
+      100
+    );
+    $("#drop_right").animate(
+      {
+        "min-width": "60px",
+        "max-width": "60px",
+      },
+      200
+    );
+    document.getElementById("points").innerHTML = "";
+  }
+  k++;
+}
+
+$("#drop_right").width($("#panel").width());
--- src/voiceChats.js
+++ src/voiceChats.js
@@ -2,13 +2,6 @@ function voice_channel_creation(text) {
   if (text == null) text = "";
   let div = document.createElement("div");
   let message = document.getElementById("input1").value;
-
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, "0");
-  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-  var yyyy = today.getFullYear();
-  var today = dd + "." + mm + "." + yyyy;
-
   div.innerHTML =
     '<div style="display: flex; margin-top: 15px;" id="' +
     Date.now() +
