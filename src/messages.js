a92bc12e2d4a8e1eb77a2cbd8faa96a8292d4f2f
Ivan <263476263476v@gmail.com>
2021-02-03 16:01:45 +0700

    messages border

--- src/messages.js
+++ src/messages.js
@@ -19,11 +19,11 @@ function message_creation(text) {
     '<div class="message" id="' +
     id +
     '"><img src="img/avatar.png" style="margin-right: 15px;" height="48px">' +
-    '<div><div style="display: flex;"><h6 class="message_text">Snolly</h6><h9 class="date">' +
+    '<div class="message_border"><div style="display: flex;"><h6 class="message_text">Snolly</h6><h9 class="date">' +
     today +
     '</h9></div><textarea id="' +
     text_id +
-    '" readonly="true" style="width: 400px; overflow: hidden; border: 0; outline-width: 0; background-color: transparent; color: white; resize: none; font-size: 0.9em;">' +
+    '" readonly="true" style="width: 250px; overflow: hidden; border: 0; outline-width: 0; background-color: transparent; color: white; resize: none; font-size: 0.9em;">' +
     message +
     text +
     "</textarea></div></div>";
